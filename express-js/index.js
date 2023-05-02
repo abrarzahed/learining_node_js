263b916fb176985b4f7b467e5fc8955ce6ba99b0
abrarzahed <abrarzahed6986@gmail.com>
2023-05-02 23:51:16 +0600

    express method

--- express-js/index.js
+++ express-js/index.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+// app.use(
+//   express.static(`${__dirname}/public/`, {
+//     index: "home.html",
+//   })
+// );
+
+const router = express.Router({
+  caseSensitive: true,
+});
+app.use(router);
+
+router.get("/about", (req, res) => {
   res.send("This is home page. Express it!");
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body);
   res.send("This is express post request");
 });
 
