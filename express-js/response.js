a11debcf88e226383de33d9a2bda5bb6a4d71441
abrarzahed <abrarzahed6986@gmail.com>
2023-05-04 13:07:53 +0600

    response methods

--- express-js/response.js
+++ express-js/response.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+/*
 app.get("/about", (req, res) => {
   //   res.send("About Page");
   console.log(res.headersSent);
@@ -11,6 +12,64 @@ app.get("/about", (req, res) => {
     country: "Bangladesh",
   });
   console.log(res.headersSent);
+});
+*/
+
+/*
+app.get("/about", (req, res) => {
+  res.send(data);
+  res.end(data);
+  res.json({ name: "Abrar", age: 20 });
+  res.sendStatus(404);
+});
+*/
+
+/* 
+  COMMENT: res.format()
+*/
+/*
+app.get("/about", (req, res) => {
+  res.format({
+    "text/plain": () => {
+      res.send("Hi plain text");
+    },
+    "text/html": () => {
+      res.render("pages/about", {
+        country: "Bangladesh",
+      });
+    },
+    "application/json": () => {
+      res.json({
+        message: "About",
+      });
+    },
+    default: () => {
+      res.status(406).send("Not acceptable");
+    },
+  });
+});
+*/
+
+/* 
+  COMMENT: res.cookies
+*/
+app.get("/test", (req, res) => {
+  res.send("TEST route");
+});
+
+/*
+app.get("/about", (req, res) => {
+  res.cookie("name", "ABRARHUSSENZAHED");
+  res.location("/test");
+  res.redirect("/test");
+  res.end();
+});
+*/
+
+app.get("/about", (req, res) => {
+  res.set("Title", "LWS Node");
+  console.log(res.get("Title"));
+  res.end();
 });
 
 app.listen(3000, () => {
